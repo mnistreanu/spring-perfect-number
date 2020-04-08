ddafe3b0a9318bc02c4cbe8c82ef1ddd787bb159
mnistreanu <nistreanu_maxim@mail.ru>
2020-04-08 13:00:32 +0300

    configured spring boot test with random enviroment port

--- src/test/java/com/maxn/springperfectnumber/PerfectNumberRestTests.java
+++ src/test/java/com/maxn/springperfectnumber/PerfectNumberRestTests.java
@@ -2,39 +2,35 @@ package com.maxn.springperfectnumber;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PerfectNumberRestTests {
-    private static final String serverUrl = "http://localhost:8080";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void testPerfectNumber6() throws URISyntaxException {
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri = new URI(serverUrl + "/perfect-number/6");
-
-        ResponseEntity<Boolean> responseEntity = restTemplate.getForEntity(uri, Boolean.class);
+    public void testPerfectNumber6() {
+        ResponseEntity<Boolean> responseEntity = restTemplate.getForEntity("/perfect-number/6", Boolean.class);
         Assert.assertTrue(responseEntity.getBody());
     }
 
     @Test
-    public void testNotPerfectNumber1() throws URISyntaxException {
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri = new URI(serverUrl + "/perfect-number/1");
-
-        ResponseEntity<Boolean> responseEntity = restTemplate.getForEntity(uri, Boolean.class);
+    public void testNotPerfectNumber1() {
+        ResponseEntity<Boolean> responseEntity = restTemplate.getForEntity("/perfect-number/1", Boolean.class);
         Assert.assertFalse(responseEntity.getBody());
     }
 
     @Test
-    public void testPerfectNumberRange() throws URISyntaxException {
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri = new URI(serverUrl + "/perfect-number-range?start=0&end=1000");
-        ResponseEntity<Integer[]> responseEntity = restTemplate.getForEntity(uri, Integer[].class);
+    public void testPerfectNumberRange() {
+        ResponseEntity<Integer[]> responseEntity = restTemplate.getForEntity("/perfect-number-range?start=0&end=1000", Integer[].class);
 
         Integer[] expectedNumbers = {6, 28, 496};
         Assert.assertArrayEquals(expectedNumbers, responseEntity.getBody());
